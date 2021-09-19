c785a08c45a31708be612155b4b62bbfca834377
Ahmet Durur <ahmet.durur@outlook.com>
2021-09-20 01:08:52 +0300

    İlk commit

---
+++ src/main/java/com/folksdev/account/model/Customer.kt
@@ -0,0 +1,44 @@
+package com.folksdev.account.model
+
+import org.hibernate.annotations.GenericGenerator
+import javax.persistence.*
+
+@Entity
+data class Customer(
+        @Id
+        @GeneratedValue(generator = "UUID")
+        @GenericGenerator(name = "UUID" , strategy = "org.hibernate.id.UUIDGenerator")
+        val id: String?,
+
+        val name:String,
+        val surName:String,
+
+        @OneToMany(mappedBy = "customer" , fetch = FetchType.EAGER)
+        val accounts: Set<Account>
+)
+{
+        constructor(name: String, surName: String ) : this("", name,surName, HashSet())
+
+        override fun equals(other: Any?): Boolean {
+                if (this === other) return true
+                if (javaClass != other?.javaClass) return false
+
+                other as Customer
+
+                if (id != other.id) return false
+                if (name != other.name) return false
+                if (surName != other.surName) return false
+                if (accounts != other.accounts) return false
+
+                return true
+        }
+
+        override fun hashCode(): Int {
+                var result = id?.hashCode() ?: 0
+                result = 31 * result + name.hashCode()
+                result = 31 * result + surName.hashCode()
+                return result
+        }
+
+
+}
